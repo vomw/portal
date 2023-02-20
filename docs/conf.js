787bb31d29bbc1f921a99aa7ba2ba9f6f8cda838
acer <22770640+vomw@users.noreply.github.com>
2023-02-21 04:24:56 +0800

    update config

--- docs/conf.js
+++ docs/conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '114',
+  ver: '115',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -27,7 +27,7 @@ jsproxy_config({
       },
     },
     'mysite': {
-      label: 'location.host',
+      label: 'null',
       lines: {
         [location.host]: 1,
       }
@@ -38,12 +38,12 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        'node-cfworker-2.etherdream.com': 4,
+        // 'node-cfworker-2.etherdream.com': 4,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
         'b.007.workers.dev': 1,
-        'b.hehe.workers.dev': 1,
+        // 'b.hehe.workers.dev': 1,
         'b.lulu.workers.dev': 1,
         'b.jsproxy.workers.dev': 1,
       }
@@ -88,11 +88,11 @@ jsproxy_config({
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
-    'https://www.google.com/': {
-      redir: 'https://duckduckgo.com/'
+    'https://weibo.com/': {
+      redir: 'https://twitter.com/'
     },
     'http://qq.com/': {
-      content: 'QQ Forbidden'
+      content: 'QQ is illegal! '
     },
   }
 })
